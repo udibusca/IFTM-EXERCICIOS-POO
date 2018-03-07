fb467a887c56b529b6fad6ad0874f4991b469be4
Nichols <nichols.costa@gmail.com>
2018-03-06 21:27:07 -0300

    Exercicios da aula 06-03-2018

--- Resolucao exercicios em laboratorio POO/src/lista_06_03_2018/ex2/Programa.java
+++ Resolucao exercicios em laboratorio POO/src/lista_06_03_2018/ex2/Programa.java
@@ -4,7 +4,21 @@ public class Programa {
 
 	public static void main(String[] args) {
 
-		
+		Pessoa pessoa1 = new Pessoa("Joao", "Medeiros");
+		Funcionario pessoa2 = new Funcionario(100, 1000.00, "Leonardo", "Messias");
+		Professor pessoa3 = new Professor(200, 1500.00, "Antonio", "Silva");
+
+		System.out.println(pessoa1.getNomeCompleto());
+		System.out.println(pessoa2.getNomeCompleto());
+		System.out.println(pessoa3.getNomeCompleto());
+
+		System.out.println("Informações da 2 pessoa");
+		System.out.println(pessoa2.getSalarioPrimeiraParcela());
+		System.out.println(pessoa2.getSalarioSegundaParcela());
+
+		System.out.println("Informações da 3 pessoa");
+		System.out.println(pessoa3.getSalarioPrimeiraParcela());
+		System.out.println(pessoa3.getSalarioSegundaParcela());
 
 	}
 
